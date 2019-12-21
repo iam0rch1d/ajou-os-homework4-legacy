b2968125e64cae7b7e69802bfa02b9532de60603
iam0rch1d <heavyknell@gmail.com>
2019-12-22 01:59:29 +0900

    Implement function prototypes

--- part2/virtmem.c
+++ part2/virtmem.c
@@ -1,6 +1,6 @@
 /**
  * virtmem.c 
- * Written by Junyeong Yoon
+ * Written by Junyeong Yoon 201723300
  */
 
 #include <stdio.h>
@@ -26,13 +26,13 @@
 #define BUFFER_SIZE 10
 
 struct tlbentry {
-	unsigned char logical;
-	unsigned char physical;
+	unsigned char page_num;
+	unsigned char frame_num;
 };
 
 // TLB is kept track of as a circular array, with the oldest element being overwritten once the TLB is full.
 struct tlbentry tlb[TLB_SIZE];
-// number of inserts into TLB that have been completed. Use as tlbindex % TLB_SIZE for the index of the next TLB line to use.
+// Number of inserts into TLB that have been completed. Use as tlbindex % TLB_SIZE for the index of the next TLB line to use.
 int tlbindex = 0;
 
 // pagetable[logical_page] is the physical page number for logical page. Value is -1 if that logical page isn't yet in the table.
@@ -44,7 +44,7 @@ signed char main_memory[MEMORY_SIZE];
 signed char *backing;
 
 // Function prototypes
-int check_tlb(int page_number);
+int check_tlb(int page_num);
 void add_to_tlb(int page_num, int frame_num);
 int check_pagetable(int page_num);
 void add_to_pagetable(int page_num, int frame_num);
@@ -56,17 +56,24 @@ int main(int argc, const char *argv[])
 		exit(1);
 	}
 
-	/* backing store */
+	// Backing store
 	const char *backing_filename = argv[1]; 
 	int backing_fd = open(backing_filename, O_RDONLY);
-	/* access backing store as memory through "backing" pointer */
+
+	// Access backing store as memory through "backing" pointer
 	backing = mmap(0, MEMORY_SIZE, PROT_READ, MAP_PRIVATE, backing_fd, 0); 
 
 	const char *input_filename = argv[2];
 	FILE *input_fp = fopen(input_filename, "r");
-
-	// Fill page table entries with -1 for initially empty table.
+	
 	int i;
+	// Fill all elements of the TLB entries with -1 for initially empty table
+	for (i = 0; i < TLB_SIZE; i++) {
+		tlb[i].page_num = -1;
+		tlb[i].frame_num = -1;
+	}
+
+	// Fill page table entries with -1 for initially empty table
 	for (i = 0; i < PAGES; i++) {
 		pagetable[i] = -1;
 	}
@@ -102,7 +109,7 @@ int main(int argc, const char *argv[])
 		
 		frame_num = check_tlb(page_num);
 		
-		// If the page was not found from TLB
+		// If the page was not found from the TLB
 		if (frame_num == -1) {
 			frame_num = check_pagetable(page_num);
 			
@@ -120,6 +127,12 @@ int main(int argc, const char *argv[])
 		else {
 			tlb_hits++;
 		}
+		
+		// Translate the frame into the physical address
+		physical_address = (frame_num << OFFSET_BITS) + offset;
+		
+		// Get the value
+		value = backing[frame_num];
 
 		printf("Virtual address: %d Physical address: %d Value: %d\n", logical_address, physical_address, value);
 	}
@@ -131,4 +144,36 @@ int main(int argc, const char *argv[])
 	printf("TLB Hit Rate = %.3f\n", tlb_hits / (1. * total_addresses));
 
 	return 0;
+}
+
+
+// Check if the page is in the TLB
+int check_tlb(int page_num) {
+	int i;
+	for (i = 0; i < TLB_SIZE; i++) {
+		if (tlb[i].page_num == page_num) {
+			return tlb[i].frame_num;
+		}
+	}
+	
+	// Return -1 if the page is not in the TLB
+	return -1;
+}
+
+// Add "tlb" node to the TLB
+void add_to_tlb(int page_num, int frame_num) {
+	tlb[tlbindex].page_num = page_num;
+	tlb[tlbindex].frame_num = frame_num;
+
+	tlbindex = (tlbindex + 1) % TLB_SIZE;
+}
+
+// Get the frame at given page number, return -1 if the page is empty
+int check_pagetable(int page_num) {
+	return pagetable[page_num];
+}
+
+// Add "pagetable" node the the page table
+void add_to_pagetable(int page_num, int frame_num) {
+	pagetable[page_num] = frame_num;
 }
